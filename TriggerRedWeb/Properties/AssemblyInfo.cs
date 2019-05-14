399a34d0e38dc6fef80545991a0965832592322b
Jameson75 <meganova75@gmail.com>
2019-05-14 00:04:19 -0400

    Finalized About Page.  - Fixed wording of steps.  - Fixed spacing in FAQs and GUIDE. Made Minor Fixes  - Changed "Seller Level" in Blog page (front and back)  - Fixed wording of steps. (use 'product ideas')  - Changed "Search Products" to "Find Product Ideas"  - Fixed main layout footer (add space under copyright notice).

--- TriggerRedWeb/Properties/AssemblyInfo.cs
+++ TriggerRedWeb/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TriggerOrangeWeb")]
+[assembly: AssemblyTitle("TriggerRedWeb")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hewlett-Packard Company")]
-[assembly: AssemblyProduct("TriggerOrangeWeb")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2015")]
+[assembly: AssemblyProduct("TriggerRedWeb")]
+[assembly: AssemblyCopyright("Copyright © Eugene Adams 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.125")]
+[assembly: AssemblyFileVersion("0.1.0.125")]
